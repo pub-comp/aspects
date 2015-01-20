458f01e66ff6c37350088090bbb4f00ebf5f5482
dannyvarod_cp <dd5a0369e14211a385f128a25405b173a470e69f7n6KD4hn>
2015-01-20 15:14:50 +0000

    .
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS27]$/pubcomp/Aspects;C30522

---
+++ Monitoring.UnitTests/SpinMonitorTests.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PubComp.Aspects.Monitoring.UnitTests
+{
+    [TestClass]
+    public class SpinMonitorTests
+    {
+        [TestMethod]
+        public void Test_EnterExit_WithSingleTask()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                monitor.Enter();
+                cnt++;
+                monitor.Exit();
+            }
+
+            Assert.AreEqual(numberOfIterations, cnt);
+        }
+
+        [TestMethod]
+        public void Test_InMonitor_WithSingleTask()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            for (int i = 0; i < numberOfIterations; i++)
+                monitor.InMonitor(() => cnt++);
+
+            Assert.AreEqual(numberOfIterations, cnt);
+        }
+
+        [TestMethod]
+        public void Test_InMonitor_WithDoubleEntriesSingleTask()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                monitor.InMonitor(() =>
+                {
+                    cnt++;
+
+                    monitor.InMonitor(() =>
+                    {
+                        cnt++;
+                    });
+                });
+            }
+
+            Assert.AreEqual(numberOfIterations * 2, cnt);
+        }
+
+        [TestMethod]
+        public void Test_InMonitor_WithDoubleEntriesSingleTask_ExceptionInInner()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                try
+                {
+                    monitor.InMonitor(() =>
+                    {
+                        cnt++;
+
+                        monitor.InMonitor(() =>
+                        {
+                            cnt++;
+                            throw new ApplicationException();
+                        });
+                    });
+                }
+                catch (ApplicationException)
+                {
+                }
+            }
+
+            Assert.AreEqual(numberOfIterations * 2, cnt);
+        }
+
+        [TestMethod]
+        public void Test_InMonitor_WithDoubleEntriesSingleTask_ExceptionInOuter()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                try
+                {
+                    monitor.InMonitor(() =>
+                    {
+                        cnt++;
+
+                        monitor.InMonitor(() =>
+                        {
+                            cnt++;
+                        });
+
+                        throw new ApplicationException();
+                    });
+                }
+                catch (ApplicationException)
+                {
+                }
+            }
+
+            Assert.AreEqual(numberOfIterations * 2, cnt);
+        }
+
+        [TestMethod]
+        public void Test_EnterExit_WithMultipleTasks()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            Parallel.For(0, numberOfIterations, i =>
+            {
+                monitor.Enter();
+                cnt++;
+                monitor.Exit();
+            });
+
+            Assert.AreEqual(numberOfIterations, cnt);
+        }
+
+        [TestMethod]
+        public void Test_InMonitor_WithMultipleTasks()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            Parallel.For(0, numberOfIterations, i =>
+                monitor.InMonitor(() => cnt++));
+
+            Assert.AreEqual(numberOfIterations, cnt);
+        }
+
+        [TestMethod]
+        public void Test_InMonitor_WithDoubleEntriesMultipleTasks()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            Parallel.For(0, numberOfIterations, i =>
+                monitor.InMonitor(() =>
+                {
+                    cnt++;
+
+                    monitor.InMonitor(() =>
+                    {
+                        cnt++;
+                    });
+                }));
+
+            Assert.AreEqual(numberOfIterations * 2, cnt);
+        }
+
+        [TestMethod]
+        public void Test_InMonitor_WithDoubleEntriesMultipleTasks_ExceptionInInner()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            Parallel.For(0, numberOfIterations, i =>
+            {
+                try
+                {
+                    monitor.InMonitor(() =>
+                    {
+                        cnt++;
+
+                        monitor.InMonitor(() =>
+                        {
+                            cnt++;
+                            throw new ApplicationException();
+                        });
+                    });
+                }
+                catch (ApplicationException)
+                {
+                }
+            });
+
+            Assert.AreEqual(numberOfIterations * 2, cnt);
+        }
+
+        [TestMethod]
+        public void Test_InMonitor_WithDoubleEntriesMultipleTasks_ExceptionInOuter()
+        {
+            const int numberOfIterations = 200;
+            int cnt = 0;
+            var monitor = new SpinMonitor();
+
+            Parallel.For(0, numberOfIterations, i =>
+            {
+                try
+                {
+                    monitor.InMonitor(() =>
+                    {
+                        cnt++;
+
+                        monitor.InMonitor(() =>
+                        {
+                            cnt++;
+                        });
+
+                        throw new ApplicationException();
+                    });
+                }
+                catch (ApplicationException)
+                {
+                }
+            });
+
+            Assert.AreEqual(numberOfIterations * 2, cnt);
+        }
+    }
+}
