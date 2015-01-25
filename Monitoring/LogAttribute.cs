ea809215167b0a944d1c1c411839e9697ea4196c
dannyvarod_cp <dd5a0369e14211a385f128a25405b173a470e69f7n6KD4hn>
2015-01-25 11:23:13 +0000

    v1.1.0
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS27]$/pubcomp/Aspects;C30550

--- Monitoring/LogAttribute.cs
+++ Monitoring/LogAttribute.cs
@@ -12,11 +12,19 @@ namespace PubComp.Aspects.Monitoring
     {
         private string className;
         private string fullMethodName;
-        private readonly LogLevel exceptionLogLevel;
         private string logName;
         private ILog log;
         private readonly bool doLogValuesOnException;
+        private readonly bool doLogValuesOnEnterExit;
         private int initialized = 0;
+        [NonSerialized]
+        private Action<string> logEnterExit;
+        [NonSerialized]
+        private Action<string, Exception> logException;
+        private string enterMessage;
+        private string exitMessage;
+        private readonly LogLevel exceptionLogLevel;
+        private readonly LogLevel enterExistLogLevel;
 
         /// <summary>
         /// Creates a new LogAttribute
@@ -24,15 +32,21 @@ namespace PubComp.Aspects.Monitoring
         /// <param name="logName">Name of logger (from Common.Logging) to use, defaults to full class name of decorated class</param>
         /// <param name="exceptionLogLevel">Log level to use in case of exception, defaults to Error</param>
         /// <param name="doLogValuesOnException">Do log values of parameters passed to method in case of exception, defaults to true</param>
+        /// <param name="enterExistLogLevel">Log level to use for method enter/exit, defaults to Trace</param>
+        /// <param name="doLogValuesOnEnterExit">Do log values of parameters passed to method on enter/exit, defaults to false></param>
         /// <remarks>
         /// Entries and exists are logged with Trace log level.
         /// Exceptions are rethrown (using throw;)
         /// </remarks>
-        public LogAttribute(string logName = null, LogLevel exceptionLogLevel = LogLevel.Error, bool doLogValuesOnException = true)
+        public LogAttribute(string logName = null,
+            LogLevel exceptionLogLevel = LogLevel.Error, bool doLogValuesOnException = true,
+            LogLevel enterExistLogLevel = LogLevel.Trace, bool doLogValuesOnEnterExit = false)
         {
             this.logName = logName;
-            this.exceptionLogLevel = exceptionLogLevel;
             this.doLogValuesOnException = doLogValuesOnException;
+            this.doLogValuesOnEnterExit = doLogValuesOnEnterExit;
+            this.exceptionLogLevel = exceptionLogLevel;
+            this.enterExistLogLevel = enterExistLogLevel;
         }
 
         public override void CompileTimeInitialize(System.Reflection.MethodBase method, AspectInfo aspectInfo)
@@ -46,12 +60,65 @@ namespace PubComp.Aspects.Monitoring
             var parameterTypes = string.Join(", ", method.GetParameters().Select(p => p.ParameterType.FullName).ToArray());
 
             this.fullMethodName = string.Concat(className, '.', method.Name, '(', parameterTypes, ')');
+
+            this.enterMessage = string.Concat("Entering method: ", this.fullMethodName);
+            this.exitMessage = string.Concat("Exiting method: ", this.fullMethodName);
+        }
+
+        private void InitializeLogger()
+        {
+            this.log = LogManager.GetLogger(this.logName);
+            switch (enterExistLogLevel)
+            {
+                case LogLevel.Fatal:
+                    logEnterExit = msg => this.log.Fatal(msg);
+                    break;
+                case LogLevel.Error:
+                    logEnterExit = msg => this.log.Error(msg);
+                    break;
+                case LogLevel.Warn:
+                    logEnterExit = msg => this.log.Warn(msg);
+                    break;
+                case LogLevel.Info:
+                    logEnterExit = msg => this.log.Info(msg);
+                    break;
+                case LogLevel.Debug:
+                    logEnterExit = msg => this.log.Debug(msg);
+                    break;
+                default:
+                    logEnterExit = msg => this.log.Trace(msg);
+                    break;
+            }
+
+            switch (exceptionLogLevel)
+            {
+                case LogLevel.Fatal:
+                    logException = (msg, ex) => this.log.Fatal(msg, ex);
+                    break;
+                case LogLevel.Error:
+                    logException = (msg, ex) => this.log.Error(msg, ex);
+                    break;
+                case LogLevel.Warn:
+                    logException = (msg, ex) => this.log.Warn(msg, ex);
+                    break;
+                case LogLevel.Info:
+                    logException = (msg, ex) => this.log.Info(msg, ex);
+                    break;
+                case LogLevel.Debug:
+                    logException = (msg, ex) => this.log.Debug(msg, ex);
+                    break;
+                default:
+                    logException = (msg, ex) => this.log.Trace(msg, ex);
+                    break;
+            }
         }
 
         public override void OnInvoke(MethodInterceptionArgs args)
         {
             if (Interlocked.CompareExchange(ref initialized, 1, 0) == 0)
-                this.log = LogManager.GetLogger(this.logName);
+            {
+                InitializeLogger();
+            }
 
             if (this.log == null)
             {
@@ -59,13 +126,26 @@ namespace PubComp.Aspects.Monitoring
                 return;
             }
 
-            log.Trace(string.Concat("Entering method: ", this.fullMethodName));
+            string enter, exit;
+            if (this.doLogValuesOnEnterExit)
+            {
+                var values = JsonConvert.SerializeObject(args.Arguments.ToArray());
+                enter = string.Concat(this.enterMessage, ", values: ", values);
+                exit = string.Concat(this.exitMessage, ", values: ", values);
+            }
+            else
+            {
+                enter = this.enterMessage;
+                exit = this.exitMessage;
+            }
+
+            this.logEnterExit(enter);
 
             try
             {
                 base.OnInvoke(args);
 
-                log.Trace(string.Concat("Exiting method: ", this.fullMethodName));
+                this.logEnterExit(exit);
             }
             catch (Exception ex)
             {
@@ -74,27 +154,7 @@ namespace PubComp.Aspects.Monitoring
                             JsonConvert.SerializeObject(args.Arguments.ToArray()))
                     : string.Concat("Exception in method: ", this.fullMethodName);
 
-                switch (this.exceptionLogLevel)
-                {
-                    case LogLevel.Fatal:
-                        log.Fatal(message, ex);
-                        break;
-                    case LogLevel.Error:
-                        log.Error(message, ex);
-                        break;
-                    case LogLevel.Warn:
-                        log.Warn(message, ex);
-                        break;
-                    case LogLevel.Info:
-                        log.Info(message, ex);
-                        break;
-                    case LogLevel.Debug:
-                        log.Debug(message, ex);
-                        break;
-                    default:
-                        log.Trace(message, ex);
-                        break;
-                }
+                this.logException(message, ex);
 
                 throw;
             }
--- Monitoring/MonitorAttribute.cs
+++ Monitoring/MonitorAttribute.cs
@@ -18,11 +18,19 @@ namespace PubComp.Aspects.Monitoring
 
         #region Logging
 
-        private readonly LogLevel exceptionLogLevel;
         private string logName;
         private ILog log;
         private readonly bool doLogValuesOnException;
+        private readonly bool doLogValuesOnEnterExit;
         private int initialized = 0;
+        [NonSerialized]
+        private Action<string> logEnterExit;
+        [NonSerialized]
+        private Action<string, Exception> logException;
+        private string enterMessage;
+        private string exitMessage;
+        private readonly LogLevel exceptionLogLevel;
+        private readonly LogLevel enterExistLogLevel;
 
         #endregion
 
@@ -64,28 +72,85 @@ namespace PubComp.Aspects.Monitoring
         /// <param name="logName">Name of logger (from Common.Logging) to use, defaults to full class name of decorated class</param>
         /// <param name="exceptionLogLevel">Log level to use in case of exception, defaults to Error</param>
         /// <param name="doLogValuesOnException">Do log values of parameters passed to method in case of exception, defaults to true</param>
+        /// <param name="enterExistLogLevel">Log level to use for method enter/exit, defaults to Trace</param>
+        /// <param name="doLogValuesOnEnterExit">Do log values of parameters passed to method on enter/exit, defaults to false></param>
         /// <remarks>
         /// Entries and exists are logged with Trace log level.
         /// Exceptions are rethrown (using throw;)
         /// </remarks>
-        public MonitorAttribute(string logName = null, LogLevel exceptionLogLevel = LogLevel.Error, bool doLogValuesOnException = true)
+        public MonitorAttribute(string logName = null,
+            LogLevel exceptionLogLevel = LogLevel.Error, bool doLogValuesOnException = true,
+            LogLevel enterExistLogLevel = LogLevel.Trace, bool doLogValuesOnEnterExit = false)
         {
             this.logName = logName;
-            this.exceptionLogLevel = exceptionLogLevel;
             this.doLogValuesOnException = doLogValuesOnException;
+            this.doLogValuesOnEnterExit = doLogValuesOnEnterExit;
+            this.exceptionLogLevel = exceptionLogLevel;
+            this.enterExistLogLevel = enterExistLogLevel;
         }
 
         public override void CompileTimeInitialize(System.Reflection.MethodBase method, AspectInfo aspectInfo)
         {
             // ReSharper disable once PossibleNullReferenceException
-            className = method.DeclaringType.FullName;
-
+            this.className = method.DeclaringType.FullName;
+            
             if (string.IsNullOrEmpty(this.logName))
                 this.logName = this.className;
 
             var parameterTypes = string.Join(", ", method.GetParameters().Select(p => p.ParameterType.FullName).ToArray());
 
             this.fullMethodName = string.Concat(className, '.', method.Name, '(', parameterTypes, ')');
+
+            this.enterMessage = string.Concat("Entering method: ", this.fullMethodName);
+            this.exitMessage = string.Concat("Exiting method: ", this.fullMethodName);
+        }
+
+        private void InitializeLogger()
+        {
+            this.log = LogManager.GetLogger(this.logName);
+            switch (enterExistLogLevel)
+            {
+                case LogLevel.Fatal:
+                    logEnterExit = msg => this.log.Fatal(msg);
+                    break;
+                case LogLevel.Error:
+                    logEnterExit = msg => this.log.Error(msg);
+                    break;
+                case LogLevel.Warn:
+                    logEnterExit = msg => this.log.Warn(msg);
+                    break;
+                case LogLevel.Info:
+                    logEnterExit = msg => this.log.Info(msg);
+                    break;
+                case LogLevel.Debug:
+                    logEnterExit = msg => this.log.Debug(msg);
+                    break;
+                default:
+                    logEnterExit = msg => this.log.Trace(msg);
+                    break;
+            }
+
+            switch (exceptionLogLevel)
+            {
+                case LogLevel.Fatal:
+                    logException = (msg, ex) => this.log.Fatal(msg, ex);
+                    break;
+                case LogLevel.Error:
+                    logException = (msg, ex) => this.log.Error(msg, ex);
+                    break;
+                case LogLevel.Warn:
+                    logException = (msg, ex) => this.log.Warn(msg, ex);
+                    break;
+                case LogLevel.Info:
+                    logException = (msg, ex) => this.log.Info(msg, ex);
+                    break;
+                case LogLevel.Debug:
+                    logException = (msg, ex) => this.log.Debug(msg, ex);
+                    break;
+                default:
+                    logException = (msg, ex) => this.log.Trace(msg, ex);
+                    break;
+            }
         }
 
         public override void OnInvoke(MethodInterceptionArgs args)
@@ -97,10 +162,9 @@ namespace PubComp.Aspects.Monitoring
             state.IncrementEntries();
 
             if (Interlocked.CompareExchange(ref initialized, 1, 0) == 0)
-                this.log = LogManager.GetLogger(this.logName);
-
-            if (this.log == null)
-                this.log = LogManager.GetLogger(className);
+            {
+                InitializeLogger();
+            }
 
             if (this.log == null)
             {
@@ -126,7 +190,20 @@ namespace PubComp.Aspects.Monitoring
                 }
             }
 
-            log.Trace(string.Concat("Entering method: ", this.fullMethodName));
+            string enter, exit;
+            if (this.doLogValuesOnEnterExit)
+            {
+                var values = JsonConvert.SerializeObject(args.Arguments.ToArray());
+                enter = string.Concat(this.enterMessage, ", values: ", values);
+                exit = string.Concat(this.exitMessage, ", values: ", values);
+            }
+            else
+            {
+                enter = this.enterMessage;
+                exit = this.exitMessage;
+            }
+
+            this.logEnterExit(enter);
 
             stopwatch.Start();
 
@@ -138,7 +215,7 @@ namespace PubComp.Aspects.Monitoring
                 stopwatch.Stop();
                 state.UpdateStatistics(elapsedMilliseconds, false);
 
-                log.Trace(string.Concat("Exiting method: ", this.fullMethodName));
+                this.logEnterExit(exit);
             }
             catch (Exception ex)
             {
@@ -151,27 +228,7 @@ namespace PubComp.Aspects.Monitoring
                             JsonConvert.SerializeObject(args.Arguments.ToArray()))
                     : string.Concat("Exception in method: ", this.fullMethodName);
 
-                switch (this.exceptionLogLevel)
-                {
-                    case LogLevel.Fatal:
-                        log.Fatal(message, ex);
-                        break;
-                    case LogLevel.Error:
-                        log.Error(message, ex);
-                        break;
-                    case LogLevel.Warn:
-                        log.Warn(message, ex);
-                        break;
-                    case LogLevel.Info:
-                        log.Info(message, ex);
-                        break;
-                    case LogLevel.Debug:
-                        log.Debug(message, ex);
-                        break;
-                    default:
-                        log.Trace(message, ex);
-                        break;
-                }
+                this.logException(message, ex);
 
                 throw;
             }
